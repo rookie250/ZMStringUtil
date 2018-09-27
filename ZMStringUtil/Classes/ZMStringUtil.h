348bace77bb5c8e1642b00a1af864f1f67945629
周明 <mmix1009@163.com>
2018-09-27 15:11:14 +0800

    add classes

---
+++ ZMStringUtil/Classes/ZMStringUtil.h
@@ -0,0 +1,20 @@
+//
+//  ZMStringUtil.h
+//  Pods
+//
+//  Created by 周明 on 2018/9/27.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZMStringUtil : NSObject
++ (BOOL)isChinese:(NSString *)chinese;
++ (BOOL)isNumber:(NSString *)number;
++ (BOOL)isLetter:(NSString *)letter;
++ (BOOL)isEmail:(NSString *)email;
++ (BOOL)isNumAndLetter:(NSString *)numAndLetter;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZMStringUtil/Classes/ZMStringUtil.m
@@ -0,0 +1,64 @@
+//
+//  ZMStringUtil.m
+//  Pods
+//
+//  Created by 周明 on 2018/9/27.
+//
+
+#import "ZMStringUtil.h"
+
+@implementation ZMStringUtil
+
++ (BOOL)isChinese:(NSString *)chinese {
+    if (chinese.length == 0) return NO;
+    NSString *regex = @"[\u4e00-\u9fa5]+";
+    NSPredicate *pred = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    return [pred evaluateWithObject:chinese];
+}
+
++ (BOOL)isNumber:(NSString *)number {
+    if (number.length == 0) {
+        return NO;
+    }
+    NSString *regex = @"[0-9]*";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    if ([predicate evaluateWithObject:number]) {
+        return YES;
+    }
+    return NO;
+}
+
++ (BOOL)isLetter:(NSString *)letter {
+    if(letter.length == 0){
+        return NO;
+    }
+    NSString *regex = @"[a-zA-Z]*";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    if ([predicate evaluateWithObject:letter]) {
+        return YES;
+    }
+    return NO;
+}
+
++ (BOOL)isEmail:(NSString *)email {
+    if(email.length == 0){
+        return NO;
+    }
+    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",emailRegex];
+    return [emailTest evaluateWithObject:email];
+}
+
++ (BOOL)isNumAndLetter:(NSString *)numAndLetter {
+    if (numAndLetter.length == 0) {
+        return NO;
+    }
+    NSString *regex = @"[a-zA-Z0-9]*";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    if ([predicate evaluateWithObject:numAndLetter]) {
+        return YES;
+    }
+    return NO;
+}
+
+@end
